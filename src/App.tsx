85d0549a77c752791004015c53b6a7ae3606da67
john-masters <mastersjohnr@gmail.com>
2023-03-22 20:02:41 +1100

    Fixed some bugs, but still have bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,41 @@
-import { useState } from 'react'
-
 export default () => {
-  const [file, setFile] = useState(null)
   
   async function handleSubmit(e:any) {
     e.preventDefault()
 
-    // const reader = new FileReader()
     const file = e.target[0].files[0]
+    const reader = new FileReader()
 
-    const fileURL = URL.createObjectURL(file)
-
-    const data = new FormData()
-    data.append('file', file)
-
-    // reader.addEventListener('load', async () => {
+    reader.addEventListener('load', async () => {
       try {
-        const res = await fetch("http://localhost:8080/", {
-          method: "POST",
-          // headers: {"Content-Type": "multipart/form-data"},
-          // body: reader.result
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({fileURL})
+        const res = await fetch('http://localhost:8080', {
+          method: 'POST',
+          headers: {'Content-Type': 'multipart/form-data'},
+          body: reader.result
         })
-        console.log("res: ", res)
+        console.log('res: ', res)
       }
 
       catch (err) {
-        console.log("err: ", err)
+        console.log('err: ', err)
       }
       // console.log(reader.result?.toString())
-    // })
-    // reader.readAsBinaryString(file)
+    })
+    reader.readAsBinaryString(file)
 
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Transcribe</h1>
       <form onSubmit={handleSubmit}>
           <input
-            type="file"
-            placeholder="Path to file"
-            accept=".mp3, .mp4, .mpeg, .mpga, .m4a, .wav, .webm"
+            id='file'
+            type='file'
+            placeholder='Path to file'
+            accept='.mp3, .mp4, .mpeg, .mpga, .m4a, .wav, .webm'
           />
-        <input id="submit" type="submit" value="Transcribe"/>
+        <input id='submit' type='submit' value='Transcribe'/>
       </form>
     </div>
   )
