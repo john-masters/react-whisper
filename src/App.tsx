94c3907ffee37e710d0987f9afb49f20b49f1156
john-masters <mastersjohnr@gmail.com>
2023-03-24 08:25:24 +1100

    Multer implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,67 @@ export default () => {
   
 // TODO: Add error handling for handleSubmit
 
-  async function handleSubmit(e:any) {
-    e.preventDefault()
+  // async function handleSubmit(e:any) {
+  //   e.preventDefault()
 
+  //   const file = e.target[0].files[0]
+  //   if (!file) return
+
+  //   const reader = new FileReader()
+
+  //   reader.addEventListener('load', async () => {
+
+  //     try {
+  //       // const boundary = Math.random().toString().slice(2);
+  //       const boundary = uuidv4()
+  //       const data = new FormData()
+  //       const blob = new Blob([reader.result], {type: file.type})
+  //       data.append('file', blob)
+
+  //       const res = await fetch('http://localhost:8080', {
+  //         method: 'POST',
+  //         headers: {'Content-Type': `multipart/form-data; boundary=${boundary}`},
+  //         // body: reader.result
+  //         body: data
+  //       })
+  //       console.log('res: ', res)
+  //     }
+
+  //     catch (err) {
+  //       console.log('err: ', err)
+  //     }
+
+  //     // console.log(reader.result)
+  //     // console.log(reader.result?.toString())
+  //   })
+  //   // console.log(file)
+  //   reader.readAsArrayBuffer(file)
+  // }
+
+  let file:any
+
+  const handleSubmit = () => {
+    console.log("submitted")
+
+    const data = new FormData()
+    data.append('file', file)
+
+    try {
+      const res = fetch('http://localhost:8080', {
+        method: 'POST',
+        body: data
+      })
+      console.log('res: ', res)
+    }
+    catch (err) {
+      console.log('err: ', err)
+    }
+
+  }
+
+  const handleChange = (e:any) => {
+    console.log("changed")
     const file = e.target[0].files[0]
-    if (!file) return
-
-    const reader = new FileReader()
-
-    reader.addEventListener('load', async () => {
-      try {
-        // const boundary = Math.random().toString().slice(2);
-        const boundary = uuidv4()
-        const res = await fetch('http://localhost:8080', {
-          method: 'POST',
-          headers: {'Content-Type': `multipart/form-data; boundary=${boundary}`},
-          body: reader.result
-        })
-        console.log('res: ', res)
-      }
-
-      catch (err) {
-        console.log('err: ', err)
-      }
-      // console.log(reader.result?.toString())
-    })
-    // console.log(file)
-    reader.readAsArrayBuffer(file)
 
   }
 
@@ -37,11 +71,14 @@ export default () => {
     <div className='App'>
       <h1>Transcribe</h1>
       <form onSubmit={handleSubmit}>
+      {/* <form encType="multipart/form-data" action="http://localhost:8080/" method="POST"> */}
           <input
             id='file'
             type='file'
+            name='file'
             placeholder='Path to file'
             accept='.mp3, .mp4, .mpeg, .mpga, .m4a, .wav, .webm'
+            onChange={handleChange}
           />
         <input id='submit' type='submit' value='Transcribe'/>
       </form>
