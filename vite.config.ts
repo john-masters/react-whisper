1284ba568fe3dfbf1b7f330a48cf3380af4bfe18
john-masters <mastersjohnr@gmail.com>
2023-03-29 22:00:36 +1100

    Fixed type issue with port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: process.env.PORT || 5173,
+    port: parseInt(process.env.PORT) || 5173,
   }
 })
