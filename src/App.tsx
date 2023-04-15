ae7a6b98fb45bf4852e004a508aad79cebb76a34
john-masters <mastersjohnr@gmail.com>
2023-04-15 22:18:44 +1000

    started implementing stripe card element

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TranscribeForm from './components/TranscribeForm'
 import Header from './components/Header'
+import PaymentForm from './components/PaymentForm'
 
 export default function App() {
   return (
     <>
       <Header />
       <TranscribeForm />
+      <PaymentForm />
     </>
   )
 }
--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { PaymentFormStyles } from './PaymentForm.styles'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import CheckoutForm from '../CheckoutForm'
+
+const promise = loadStripe("pk_test_51MpVLcJD5XPjP7WOM5mkku4D4U3WRWHgPOtwdbLQeeqtPkZjD9LK6dKiJNiSDILUnvd6vJ8QSYSeq3JNxzmwFqNf00kF49ElzP")
 
 export default function PaymentForm() {
   return (
     <PaymentFormStyles>
-      <h1>Payment Form</h1>
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
     </PaymentFormStyles>
   )
 }
