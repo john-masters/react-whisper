bc17ce23c0a8fca4cbc5a0d21403925f7bed534a
john-masters <mastersjohnr@gmail.com>
2023-04-23 19:30:39 +1000

    Stripe payments working

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -5,7 +5,8 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../CheckoutForm";
 
 const promise = loadStripe(
-  "pk_test_51MpVLcJD5XPjP7WOM5mkku4D4U3WRWHgPOtwdbLQeeqtPkZjD9LK6dKiJNiSDILUnvd6vJ8QSYSeq3JNxzmwFqNf00kF49ElzP"
+  // "pk_test_51MpVLcJD5XPjP7WOM5mkku4D4U3WRWHgPOtwdbLQeeqtPkZjD9LK6dKiJNiSDILUnvd6vJ8QSYSeq3JNxzmwFqNf00kF49ElzP" // test
+  "pk_live_51MpVLcJD5XPjP7WOUggIUwacT7CQBzKHNAzyoucXfBOX3wtTAzieiXEWBeqviAmf3ZtagfkanJF1S5d8b7Le2y0M0084HX42lS" // prod
 );
 
 interface Props {
