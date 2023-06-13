d6cdb30d91e6e8f8ad332c6d419ba0ceef5f8c76
John Masters <john.m@smithbrothersmedia.com.au>
2023-06-13 13:42:10 +1000

    Adding meta info for SEO

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,26 +5,12 @@ import { useWindowWidth } from "../../hooks/useWindowWidth";
 export default function Header() {
   const width = useWindowWidth();
 
-  // TODO: Add info about the 58 languages supported
   return (
     <HeaderStyles width={width}>
       <a href="/">
-        <h1>Scribe AI</h1>
+        <h1>Scribr</h1>
       </a>
       <p>Transforming speech to text with AI precision</p>
     </HeaderStyles>
   );
 }
-
-// Afrikaans, Arabic, Armenian, Azerbaijani, Belarusian,
-// Bosnian, Bulgarian, Catalan, Chinese, Croatian,
-// Czech, Danish, Dutch, English, Estonian,
-// Finnish, French, Galician, German, Greek,
-// Hebrew, Hindi, Hungarian, Icelandic, Indonesian,
-// Italian, Japanese, Kannada, Kazakh, Korean,
-// Latvian, Lithuanian, Macedonian, Malay, Marathi,
-// Maori, Nepali, Norwegian, Persian, Polish,
-// Portuguese, Romanian, Russian, Serbian, Slovak,
-// Slovenian, Spanish, Swahili, Swedish, Tagalog,
-// Tamil, Thai, Turkish, Ukrainian, Urdu,
-// Vietnamese, and Welsh.
